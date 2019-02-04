a9ebf1585676ba25ef08d98ebbc020d192b9531f
Samuel Levy <samulevy@gmail.com>
2019-02-04 12:40:01 -0300

    Add and configure frontend folder, react-on-rails & remove assets folder

---
+++ app/frontend/packs/application.js
@@ -0,0 +1,3 @@
+import '../stylesheets/application.scss'
+import './react-on-rails'
+import '../images'
---
+++ app/frontend/packs/react-on-rails.js
@@ -0,0 +1,10 @@
+import ReactOnRails from 'react-on-rails';
+import HelloWorld from '../components/HelloWorld';
+import PagesHome from '../templates/pages/PagesHome';
+
+ReactOnRails.register({
+  HelloWorld,
+  PagesHome,
+});
+
+reactOnRails()
